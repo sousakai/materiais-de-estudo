36be349d5dc79c5e058ae568c8bb52dae749fd3d
Kayke de Sousa <kayke.gsousa15@gmail.com>
2025-04-28 13:44:41 -0300

    Uso de ponteiros com vetor
    Uso de ponteiros para manipular e exibir elementos de um vetor de 5 inteiros.

---
+++ estrutura-de-dados-I/ponteiros/ponteiros-vetor.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Estrutura de Dados I 
+Prof.º Eduardo Alves de Freitas
+
+Desenvolva um programa em C crie um vetor de inteiros
+com tamanho 5, atribua valores no vetor no momento de
+sua declaração. Crie um ponteiro de inteiros, atribua o
+endereço do vetor no ponteiro.
+Percorra todos os elementos do vetor utilizando o ponteiro e
+incremente o valor 1 em cada elemento do vetor.
+Mostre os valores do vetor utilizando o ponteiro.
+*******************************************************************************/
+#include <stdio.h>
+
+
+
+int main(){
+    int vetor[5] = {10, 20, 30, 40, 50};
+    int i, *p;
+    p = vetor;  // atribui o endereço do primeiro elemento do vetor à variável p
+    
+      // 'p' agora aponta para o primeiro elemento do vetor, então usamos *p para acessar o valor nele.
+      
+    printf("Valores do vetor:\n");
+    for(i = 0; i < 5; i++){
+       *p +=1;
+       printf("%d\t", *p);
+       p++; // movemos o ponteiro para a próxima casa do vetor
+    }
+    
+}
